bba652b62875393bd73c0d0231509464c728a04e
Daniel Pope <djpope2365@gmail.com>
2014-01-26 13:04:40 +0000

    Text.py added to write text with LEDs

--- animation.py
+++ animation.py
@@ -1,28 +1,59 @@
 from time import sleep
 from time import time
 import RPi.GPIO as GPIO
+import sys
+import pickle
 
 #Setting stuff up
 leds = [24, 23, 22, 21, 18, 17]
 buttons = [25, 4]
 
+argc = len(sys.argv)
+argv = sys.argv
+
+animation = []
+
 sample = []
-sample.append([[leds[0],0.6],[leds[1],0],[leds[2],0],[leds[3],0],[leds[4],0],[leds[5],0.200000000000001]])
-sample.append([[leds[0],0.55],[leds[1],1],[leds[2],0],[leds[3],0],[leds[4],0],[leds[5],0.150000000000001]])
-sample.append([[leds[0],0.5],[leds[1],0.5],[leds[2],1],[leds[3],0],[leds[4],0],[leds[5],0.100000000000001]])
-sample.append([[leds[0],0.45],[leds[1],0.2],[leds[2],0.5],[leds[3],1],[leds[4],0],[leds[5],0.0500000000000009]])
-sample.append([[leds[0],0.4],[leds[1],0],[leds[2],0.2],[leds[3],0.5],[leds[4],1],[leds[5],0]])
-sample.append([[leds[0],0.35],[leds[1],0],[leds[2],0],[leds[3],0.2],[leds[4],0.5],[leds[5],0]])
-sample.append([[leds[0],0.3],[leds[1],0],[leds[2],0],[leds[3],0],[leds[4],0.2],[leds[5],0]])
-sample.append([[leds[0],0.25],[leds[1],0],[leds[2],0],[leds[3],0],[leds[4],0],[leds[5],0]])
-sample.append([[leds[0],0.200000000000001],[leds[1],0],[leds[2],0],[leds[3],0],[leds[4],0],[leds[5],0.6]])
-sample.append([[leds[0],0.150000000000001],[leds[1],0],[leds[2],0],[leds[3],0],[leds[4],1],[leds[5],0.55]])
-sample.append([[leds[0],0.100000000000001],[leds[1],0],[leds[2],0],[leds[3],1],[leds[4],0.5],[leds[5],0.5]])
-sample.append([[leds[0],0.0500000000000009],[leds[1],0],[leds[2],1],[leds[3],0.5],[leds[4],0.25],[leds[5],0.45]])
-sample.append([[leds[0],0],[leds[1],1],[leds[2],0.5],[leds[3],0.25],[leds[4],0],[leds[5],0.4]])
-sample.append([[leds[0],0],[leds[1],0.5],[leds[2],0.25],[leds[3],0],[leds[4],0],[leds[5],0.35]])
-sample.append([[leds[0],0],[leds[1],0.25],[leds[2],0],[leds[3],0],[leds[4],0],[leds[5],0.3]])
-sample.append([[leds[0],0],[leds[1],0],[leds[2],0],[leds[3],0],[leds[4],0],[leds[5],0.25]])
+sample.append([[leds[0],1],[leds[1],0],[leds[2],0],[leds[3],0],[leds[4],0],[leds[5],0]])
+sample.append([[leds[0],0.5],[leds[1],1],[leds[2],0],[leds[3],0],[leds[4],0],[leds[5],0]])
+sample.append([[leds[0],0.2],[leds[1],0.5],[leds[2],1],[leds[3],0],[leds[4],0],[leds[5],0]])
+sample.append([[leds[0],0],[leds[1],0.2],[leds[2],0.5],[leds[3],1],[leds[4],0],[leds[5],0]])
+sample.append([[leds[0],0],[leds[1],0],[leds[2],0.2],[leds[3],0.5],[leds[4],1],[leds[5],0]])
+sample.append([[leds[0],0],[leds[1],0],[leds[2],0],[leds[3],0.2],[leds[4],0.5],[leds[5],1]])
+sample.append([[leds[0],0],[leds[1],0],[leds[2],0],[leds[3],0],[leds[4],0.2],[leds[5],0.5]])
+sample.append([[leds[0],0],[leds[1],0],[leds[2],0],[leds[3],0],[leds[4],0],[leds[5],0.2]])
+sample.append([[leds[0],0],[leds[1],0],[leds[2],0],[leds[3],0],[leds[4],0],[leds[5],0]])
+sample.append([[leds[0],0],[leds[1],0],[leds[2],0],[leds[3],0],[leds[4],0],[leds[5],1]])
+sample.append([[leds[0],0],[leds[1],0],[leds[2],0],[leds[3],0],[leds[4],1],[leds[5],0.5]])
+sample.append([[leds[0],0],[leds[1],0],[leds[2],0],[leds[3],1],[leds[4],0.5],[leds[5],0.2]])
+sample.append([[leds[0],0],[leds[1],0],[leds[2],1],[leds[3],0.5],[leds[4],0.2],[leds[5],0]])
+sample.append([[leds[0],0],[leds[1],1],[leds[2],0.5],[leds[3],0.2],[leds[4],0],[leds[5],0]])
+sample.append([[leds[0],1],[leds[1],0.5],[leds[2],0.2],[leds[3],0],[leds[4],0],[leds[5],0]])
+sample.append([[leds[0],0.5],[leds[1],0.2],[leds[2],0],[leds[3],0],[leds[4],0],[leds[5],0]])
+sample.append([[leds[0],0.2],[leds[1],0],[leds[2],0],[leds[3],0],[leds[4],0],[leds[5],0]])
+sample.append([[leds[0],0],[leds[1],0],[leds[2],0],[leds[3],0],[leds[4],0],[leds[5],0]])
+
+wave = []
+wave.append([[leds[0],0.5],[leds[1],0.6],[leds[2],0.7],[leds[3],0.8],[leds[4],0.9],[leds[5],1]])
+wave.append([[leds[0],0.4],[leds[1],0.5],[leds[2],0.6],[leds[3],0.7],[leds[4],0.8],[leds[5],0.9]])
+wave.append([[leds[0],0.3],[leds[1],0.4],[leds[2],0.5],[leds[3],0.6],[leds[4],0.7],[leds[5],0.8]])
+wave.append([[leds[0],0.2],[leds[1],0.3],[leds[2],0.4],[leds[3],0.5],[leds[4],0.6],[leds[5],0.7]])
+wave.append([[leds[0],0.1],[leds[1],0.2],[leds[2],0.3],[leds[3],0.4],[leds[4],0.5],[leds[5],0.6]])
+wave.append([[leds[0],0],[leds[1],0.1],[leds[2],0.2],[leds[3],0.3],[leds[4],0.4],[leds[5],0.5]])
+wave.append([[leds[0],0.1],[leds[1],0],[leds[2],0.1],[leds[3],0.2],[leds[4],0.3],[leds[5],0.4]])
+wave.append([[leds[0],0.2],[leds[1],0.1],[leds[2],0],[leds[3],0.1],[leds[4],0.2],[leds[5],0.3]])
+wave.append([[leds[0],0.3],[leds[1],0.2],[leds[2],0.1],[leds[3],0],[leds[4],0.1],[leds[5],0.2]])
+wave.append([[leds[0],0.4],[leds[1],0.3],[leds[2],0.2],[leds[3],0.1],[leds[4],0],[leds[5],0.1]])
+wave.append([[leds[0],0.5],[leds[1],0.4],[leds[2],0.3],[leds[3],0.2],[leds[4],0.1],[leds[5],0]])
+wave.append([[leds[0],0.6],[leds[1],0.5],[leds[2],0.4],[leds[3],0.3],[leds[4],0.2],[leds[5],0.1]])
+wave.append([[leds[0],0.7],[leds[1],0.6],[leds[2],0.5],[leds[3],0.4],[leds[4],0.3],[leds[5],0.2]])
+wave.append([[leds[0],0.8],[leds[1],0.7],[leds[2],0.6],[leds[3],0.5],[leds[4],0.4],[leds[5],0.3]])
+wave.append([[leds[0],0.9],[leds[1],0.8],[leds[2],0.7],[leds[3],0.6],[leds[4],0.5],[leds[5],0.4]])
+wave.append([[leds[0],1],[leds[1],0.9],[leds[2],0.8],[leds[3],0.7],[leds[4],0.6],[leds[5],0.5]])
+wave.append([[leds[0],0.9],[leds[1],1],[leds[2],0.9],[leds[3],0.8],[leds[4],0.7],[leds[5],0.6]])
+wave.append([[leds[0],0.8],[leds[1],0.9],[leds[2],1],[leds[3],0.9],[leds[4],0.8],[leds[5],0.7]])
+wave.append([[leds[0],0.7],[leds[1],0.8],[leds[2],0.9],[leds[3],1],[leds[4],0.9],[leds[5],0.8]])
+wave.append([[leds[0],0.6],[leds[1],0.7],[leds[2],0.8],[leds[3],0.9],[leds[4],1],[leds[5],0.9]])
 
 
 GPIO.setwarnings(False)
@@ -48,19 +79,31 @@ def varyBrightness(artup, duration):
     for i in range(5):
         delays[i] = (artup[i+1][1]**2 - artup[i][1]**2) / 100.0
     cycles = int(duration * 100)
+    onLeds = []
     for i in range(cycles):
-        allLeds(True)
+        for l in range(6):
+            if artup[l][1] > 0:
+                GPIO.output(artup[l][0], True)
         for l in range(5):
             GPIO.output(artup[l][0], False)
             sleep(delays[l])
         GPIO.output(artup[5][0], False)
         sleep((1-artup[-1][1]) / 100.0)
                         
-def animate(duration):
-    frameTime = duration / float(len(sample))
+def animate(sequence, duration):
+    frameTime = duration / float(len(sequence))
     print 1.0 / frameTime, "fps"
     while True:
-        for step in sample:
+        for step in sequence:
             varyBrightness(step, frameTime)
 
-animate(1)
+if argc == 1:
+    print 'Sample animation'
+    animate(sample, 1)
+else:
+    if argv[1].endswith('.seq'):
+        animation = pickle.load(open(argv[1], "rb"))
+        animate(animation, 1)
+    else:
+        text = ' '.join(argv[1:])
+        print text
---
+++ text.py
@@ -0,0 +1,73 @@
+from time import sleep
+from time import time
+import RPi.GPIO as GPIO
+import sys
+import pickle
+
+#Setting stuff up
+leds = [24, 23, 22, 21, 18, 17]
+buttons = [25, 4]
+symbol = {}
+execfile("letters.py")
+
+argc = len(sys.argv)
+argv = sys.argv
+
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+for pin in leds:
+    GPIO.setup(pin, GPIO.OUT)
+
+for pin in buttons:
+    GPIO.setup(pin, GPIO.IN)
+########################################
+    
+def allLeds(onOrOff):
+    for pin in leds:
+        GPIO.output(pin, onOrOff)
+
+#Takes an array of tuples and sets each LED's brightness for a duration
+# [(17,0.5),(18,1),...]
+def varyBrightness(artup, duration):
+    for pin in artup:
+        if pin[1] == 1:
+            GPIO.output(pin[0], True)
+        else:
+            GPIO.output(pin[0], False)
+    sleep(duration)
+
+def changeSpeed(animation, duration, speed):
+    duration /= speed
+    frameTime = duration / float(len(animation))
+    print 1.0 / frameTime, "fps"
+    print duration, "seconds"
+    while GPIO.input(buttons[0]) == 1:
+        sleep(0.02)
+    return frameTime
+                        
+def animate(sequence, duration):
+    animation = []
+    for char in sequence:
+        animation.extend(symbol[char])
+    speed = 1
+    frameTime = changeSpeed(animation, duration, speed)
+    while True:
+        for step in animation:
+            if GPIO.input(buttons[0]) == 1:
+                if speed == 1:
+                    speed = 0.1
+                else:
+                    speed = 1
+                frameTime = changeSpeed(animation, duration, speed)
+            varyBrightness(step, frameTime)
+        #sleep(1)
+
+if argc == 1:
+    sequence = 'HELLO'
+else:
+    sequence = ' '.join(argv[1:])
+
+sequence += "             "
+animate(sequence, len(sequence) * 0.1)
